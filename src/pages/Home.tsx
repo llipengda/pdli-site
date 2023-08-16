2b0a43f2f90be5324e22625bcb27bc9c3a9ea193
llipengda <lipengda2@outlook.com>
2023-08-16 20:45:12 +0800

    add deviders in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Divider } from '@mui/material'
 import Welcome from '../components/Main/Home/Welcome/Welcome'
 import AboutMe from '../components/Main/Home/AboutMe/AboutMe'
 import Links from '../components/Main/Home/Links/Links'
@@ -9,8 +9,11 @@ const Home: React.FC = () => {
   return (
     <Box display='flex' flexDirection='column' id='home'>
       <Welcome />
+      <Divider />
       <AboutMe />
+      <Divider />
       <Links />
+      <Divider />
       <Comments />
     </Box>
   )
