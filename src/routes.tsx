0bfc21f36a8c6f1dd881885f0c9ceaca4606e318
llipengda <lipengda2@outlook.com>
2023-08-17 14:39:05 +0800

    fix case bug

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,8 @@ import { Navigate, RouteObject } from 'react-router-dom'
 import Home from './pages/Home'
 import Services from './pages/services/Services'
 import ServicesIndex from './components/Main/Services/ServicesIndex'
-import Minecraft from './pages/services/minecraft'
-import CodeServer from './pages/services/codeServer'
+import Minecraft from './pages/services/Minecraft'
+import CodeServer from './pages/services/CodeServer'
 
 const routes: RouteObject[] = [
   { path: '/home', element: <Home /> },
